80df838d52b740d128ce82fa090e7e3b490a178e
Keegan <keegan@lillo.me>
2016-02-12 16:58:22 -0800

    add tests for select component

---
+++ test/spec/components/select.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var assert = require('chai').assert;
+var fixture = require('../../fixture');
+
+describe('component - color', function() {
+  it('sets the value display to the correct value on change', function() {
+    var clayConfig = fixture.clayConfig([
+      {
+        type: 'select',
+        value: 'value-1',
+        options: [
+          { label: 'label 1', value: 'value-1' },
+          { label: 'label 2', value: 'value-2' }
+        ]
+      }
+    ]);
+    var selectItem = clayConfig.getItemsByType('select')[0];
+    var $valueDisplay = selectItem.$element.select('.value');
+    assert.strictEqual($valueDisplay.get('innerHTML'), 'label 1');
+    selectItem.set('value-2');
+    assert.strictEqual($valueDisplay.get('innerHTML'), 'label 2');
+  });
+});
