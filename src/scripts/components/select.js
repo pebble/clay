069d4a80da7f9936e0d212ffc14fdcc28a5e925b
Keegan <keegan@lillo.me>
2016-03-06 14:28:38 +1100

    only dispatch manipulator events if the state is actually changed.

--- src/scripts/components/select.js
+++ src/scripts/components/select.js
@@ -16,11 +16,18 @@ module.exports = {
 
     var $value = self.$element.select('.value');
 
-    self.on('change', function() {
+    /**
+     * Updates the HTML value of the component to match the slected option's label
+     * @return {void}
+     */
+    function setValueDisplay() {
       var selectedIndex = self.$manipulatorTarget.get('selectedIndex');
       var $options = self.$manipulatorTarget.select('option');
       var value = $options[selectedIndex] && $options[selectedIndex].innerHTML;
       $value.set('innerHTML', value);
-    });
+    }
+
+    setValueDisplay();
+    self.on('change', setValueDisplay);
   }
 };
